947b2373d390d69c91905a87ce3fb05bcb43dd26
suhasofficialy <87524054+suhasofficial@users.noreply.github.com>
2021-11-12 18:45:59 +0530

    Add files via upload

---
+++ auto.js
@@ -0,0 +1,6 @@
+    var typed = new Typed(".auto-input",{
+        strings : ["i'm suhas kuthnikar","Web Developer", "Ui designer"],
+        typeSpeed:100,
+        bacSpeed:100,
+        loop:true,
+    })
